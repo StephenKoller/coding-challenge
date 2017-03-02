ae3bbf42beca3022b2e3f19a9986de4a3692c28e
Stephen Koller <stephenkoller@gmail.com>
2017-03-01 22:56:32 -0500

    Add link admin layout

--- app/components/home.controller.js
+++ app/components/home.controller.js
@@ -2,5 +2,15 @@ angular.module('grapeviin')
 .controller('HomeController', function ($stateParams) {
   var vm = this;
 
+  vm.list = [
+    {
+      text: 'foo',
+      clicks: 0,
+    },
+    {
+      text: 'bar',
+      clicks: 1,
+    }
+  ];
   console.log("home init");
 });
