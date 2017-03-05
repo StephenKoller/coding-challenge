227dccf5046061f0ee8aa20f69bbb12867c44606
Stephen Koller <stephenkoller@gmail.com>
2017-03-05 17:43:53 -0500

    Add helpful comments

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -1,15 +1,18 @@
 angular.module('grapeviin')
 .controller('HomeController', function ($stateParams, LinkService) {
   var vm = this;
-
   vm.LinkService = LinkService;
+
+  // UI State Variables for Sorting Data Table
   vm.sortType = 'clicks';
   vm.sortReverse = true;
 
+  // Controller Init
   vm.init = function() {
     vm.LinkService.getLinks();
   };
 
+  // CRUD Operations - for a slim controller, pass calls to service layer
   vm.addLink = function(text) {
     vm.LinkService.addLink(text);
   };
@@ -25,6 +28,7 @@ angular.module('grapeviin')
   vm.saveLink = function(link, text) {
     vm.LinkService.saveLink(link, text);
   };
+  // end CRUD Operations
 
   vm.init();
 });
