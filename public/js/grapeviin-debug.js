67840008e74b7fc84b233817b64f573e8da74207
Stephen Koller <stephenkoller@gmail.com>
2017-03-07 20:16:59 -0500

    Build latest

--- public/js/grapeviin-debug.js
+++ public/js/grapeviin-debug.js
@@ -6,11 +6,11 @@ angular
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'html/home/home.view.html',
+        templateUrl: 'public/html/home/home.view.html',
         controller: 'HomeController as vm'
       })
       .when('/landing/:link', {
-        templateUrl: 'html/landing/landing.view.html',
+        templateUrl: 'public/html/landing/landing.view.html',
         controller: 'LandingController as vm'
       })
       .when('/:link', {
@@ -19,7 +19,7 @@ angular
         }
       })
       // .when('/404', {
-      //   templateUrl: 'html/404.html',
+      //   templateUrl: 'public/html/404.html',
       //   controller: function($scope) {}
       // })
       .otherwise({
